cbb897959f8a64ab4f17968bacd6619bf9a8ede0
Maxwell Knight <maxwell.knight98@gmail.com>
2022-10-10 21:39:26 +0300

    Model changes

--- models/Table.ts
+++ models/Table.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 import { IBaseShift } from '../interfaces/IShift'
 
 const tableSchema = new mongoose.Schema({
-    table: { type: Map<String, IBaseShift[]>, required: true }
+    table: { type: Map<String, IBaseShift[]>, required: true },
+    startDate: { type: Date, required: true, default: () => new Date() },
+    endDate: { type: Date, required: true, default: () => new Date() }
 })
 
 const Table = mongoose.model('Table', tableSchema)
